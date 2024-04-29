d58fed241e0ff045c53a6d2d7b57380990f6afe8
nanda-yo <104871983+nanda-yo@users.noreply.github.com>
2024-04-29 17:33:07 +0300

    added proper field randomizer

---
+++ src/generators/api_object.py
@@ -0,0 +1,65 @@
+from faker import Faker
+from uuid import uuid4
+from src.baseclasses.builder import BuilderBaseClass
+
+thefaker = Faker()
+
+class ApiObjectBuilder(BuilderBaseClass):
+    def update_key(self, key):
+        method_name = 'generate_' + key
+        fn = getattr(self, method_name)
+        if callable(fn):
+            return fn()
+        else:
+            return 'not_callable'
+
+    @classmethod
+    def generate_pk(cls):
+        return str(uuid4())
+
+    @classmethod
+    def generate_sk(cls):
+        # returns fixed value to ease up the cleaning in case something goes wrong :O
+        return str("test")
+
+    @classmethod
+    def generate_title(cls):
+        return Faker().paragraph(nb_sentences=2)
+
+    @classmethod
+    def generate_author(cls):
+        return Faker().name()
+
+    @classmethod
+    def generate_description(cls):
+        return Faker().text(max_nb_chars = 300)
+
+
+    @classmethod
+    def build(cls):
+        pk = cls.generate_pk()
+        sk = cls.generate_sk()
+        title = cls.generate_title()
+        author = cls.generate_author()
+        description = cls.generate_description()
+        var1=Faker().isbn10()
+        var2=str(Faker().isbn10())
+        var3=Faker().pyfloat()
+        params = [
+     {
+       "var1": var1,
+       "var2": var2,
+       "var3": var3
+     }
+   ]
+
+        valid_item_object = {
+          "pk": pk,
+          "sk": sk,
+          "title": title,
+          "author": author,
+          "description": description,
+          "params": params
+        }
+
+        return valid_item_object
--- tests/test_api1.py
+++ tests/test_api1.py
@@ -3,50 +3,59 @@ import requests
 import config
 from src.schemas.payload import Payload
 from src.baseclasses.response import Response
-from conftest import ValueStorage
 from uuid import uuid4
 
+
 #@pytest.mark.skip
-def test_can_call_endpoint():
+def test_can_call_endpoint(ValueStorage):
     r = requests.get(url=config.ENDPOINT_URL)
     ValueStorage.xsrf = {'csrftoken' : r.cookies.get('csrftoken')}
     assert r.status_code == 200
 
-def test_xsrf_required():
-    r=requests.post(url=config.SERVICE_URL+'create',json =ValueStorage.valid_item_object)
+def test_xsrf_required(ValueStorage):
+    r=requests.post(url=config.SERVICE_URL+'create',json = ValueStorage.valid_item_object)
     assert r.status_code == 403
 
-@pytest.mark.skip(reason="for the sake of making swagger doc page working, currently there is no such thing as header xsrf token check, it just accepts one from cookies, as long as it exists")
-def test_valid_xsrf_required():
+@pytest.mark.skip(reason='[ISSUE] double token check doesnt work yet for opendocs compatibility')
+def test_valid_xsrf_required(ValueStorage):
     r = requests.post(url=config.SERVICE_URL + 'create', headers={'csrftoken':str(uuid4())}, json=ValueStorage.valid_item_object, cookies={'csrftoken':str(uuid4())})
     assert r.status_code == 403
 
-@pytest.mark.skip(reason="this fetches entire table")
+@pytest.mark.skip(reason="fetches entire table")
 def test_get_all():
     r = requests.get(url=config.SERVICE_URL+'all')
     response = Response(r)
     response.assert_status_code(200).validate(Payload)
 
 #@pytest.mark.skip
-def test_create_item():
-    r=requests.post(url=config.SERVICE_URL+'create',headers=ValueStorage.xsrf,json =ValueStorage.valid_item_object,cookies=ValueStorage.xsrf)
-    ValueStorage.created_item_id = r.json()
+def test_create_item(ValueStorage):
+    r=requests.post(url=config.SERVICE_URL+'create',headers=ValueStorage.xsrf,json=ValueStorage.valid_item_object,cookies=ValueStorage.xsrf)
     ValueStorage.valid_item_object['pk'] = r.json()
     Response(r).assert_status_code(200)
 
 #@pytest.mark.skip
-def test_get_item():
-    r = requests.get(url=config.SERVICE_URL+'get?item_id='+ValueStorage.created_item_id)
+def test_get_item(ValueStorage):
+    r = requests.get(url=config.SERVICE_URL+'get?item_id='+ValueStorage.valid_item_object['pk'])
     Response(r).assert_status_code(200).validate(Payload).validate_created_item(ValueStorage)
+
+
+@pytest.mark.parametrize("key", [
+    "title",
+    "author",
+    "description"
+])
+
 #@pytest.mark.skip
-def test_update_item():
-    ValueStorage.valid_item_object['title'] = 'different title'
+def test_update_item(ValueStorage, key, update_item):
+    ValueStorage.valid_item_object[key] = update_item
     r = requests.put(url=config.SERVICE_URL+'update', headers=ValueStorage.xsrf, json=ValueStorage.valid_item_object,cookies=ValueStorage.xsrf)
     Response(r).assert_status_code(200)
 
 #@pytest.mark.skip
-def test_delete_item():
-    r = requests.delete(url=config.SERVICE_URL+'delete?item_pk=' + ValueStorage.created_item_id +'&item_sk='+ ValueStorage.valid_item_object['sk'], headers=ValueStorage.xsrf,cookies=ValueStorage.xsrf)
+def test_delete_item(ValueStorage):
+    r = requests.delete(url=config.SERVICE_URL+'delete?item_pk=' +
+                            ValueStorage.valid_item_object['pk'] +'&item_sk='+
+                            ValueStorage.valid_item_object['sk'], headers=ValueStorage.xsrf,cookies=ValueStorage.xsrf)
     Response(r).assert_status_code(200)
 
 
